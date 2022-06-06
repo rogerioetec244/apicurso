66050c2f56a092e34f8048b141495fa88a0ffa44
unknown <rogerio.furlan4@etec.sp.gov.br>
2022-06-06 08:18:15 -0300

    Commit inial do projeto

---
+++ src/main/java/br/com/bidu/apicurso/ApicursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bidu.apicurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bidu/apicurso/ApicursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bidu.apicurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
